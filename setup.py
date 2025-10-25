b6627ad19d8bb2fc44b529594690b0beeb7f9587
AdiBalhara <adisinghbalhara>
2025-10-25 22:50:45 +0530

    Exception Handling and logging completed

--- setup.py
+++ setup.py
@@ -7,26 +7,34 @@ It is used by setuptools(or distutils in older Oython Version) to define the con
 such as its metadata,dependencies, and more
 '''
 
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements()->List[str]:
-        """
-        This function will return List of requirement
+def get_requirements() -> List[str]:
+    requirements_lst: List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            #Read lines from file
+            lines = file.readlines()
+            ## process each line
+            for line in lines:
+                requirement = line.strip()
+                 ##ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirements_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
 
-        """
-        try:
-                with open('requirement.txt','r') as file:
-                        #Read lines from file
-                        lines=file.readlines()
-                        ## process each line 
-                        for line in lines:
-                                requirement=line.strip()
-                                ##ignore empty lines and -e .
-                                if requirement and requirement!= '-e .':
-                                        requirement_lst.append(requirement)
-        except FileNotFoundError:
-                print("requirements.txt file not found")
+    return requirements_lst
 
-print(get_requirements())                                        
+print(get_requirements())
 
+
+setup(
+        name="NetworkSecurity",
+        version="0.0.1",
+        author="Aditya",
+        author_email="adisinghbalhara@gamil.com",
+        packages=find_packages(),
+        install_requires=get_requirements()
+)
